58c2233d50e22796917498d77655eb3080be269e
Jalil <jalilmarkel1992@gmail.com>
2019-01-02 08:36:49 -0500

    created repo

---
+++ MainApp.cs
@@ -0,0 +1,58 @@
+﻿using PCKLIB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformTemplate
+{
+    static class MainApp
+    {
+        public static MainFrm g_main_frm;
+        public static object g_locker = new object();
+        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static UserSetting g_setting;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            g_main_frm = new MainFrm();
+            Application.Run(g_main_frm);
+        }
+
+        public static void log(string msg, bool msgbox = false)
+        {
+            lock (g_locker)
+            {
+                try
+                {
+                    logger.Error(msg);
+                    if (msgbox)
+                        MessageBox.Show(msg);
+                    g_main_frm.log(msg);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public static void load_setting()
+        {
+            g_setting = UserSetting.Load();
+            if(g_setting == null)
+            {
+                g_setting = new UserSetting();
+            }
+        }
+
+        public static void save_setting()
+        {
+            g_setting.Save();
+        }
+    }
+}
---
+++ MainFrm.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using MaterialSkin.Controls;
+using MaterialSkin.Animations;
+using MaterialSkin;
+
+using PCKLIB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WinformTemplate
+{
+    public partial class MainFrm : MaterialForm
+    {
+        MaterialSkinManager skinman;
+        public MainFrm()
+        {
+            InitializeComponent();
+
+            skinman = MaterialSkinManager.Instance;
+            skinman.AddFormToManage(this);
+            skinman.Theme = MaterialSkinManager.Themes.DARK;
+            skinman.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.Blue200, Accent.Teal200, TextShade.WHITE);
+        }
+
+        #region ResizeGrip
+        private const int cGrip = 1;      // Grip size
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+        #endregion
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            MainApp.log("Program started");
+        }
+
+        public void log(string msg)
+        {
+            this.InvokeOnUiThreadIfRequired(() =>
+            {
+                string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
+                lab_status.Text = log_content;
+            });
+        }
+    }
+}
