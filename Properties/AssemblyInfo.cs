a9de1987c2a7fcecc9477b458ed63da4c1dba805
Jalil <jalilmarkel1992@gmail.com>
2019-01-12 19:38:38 -0500

    As discussed in the meeting, changed logic at  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,18 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("WinformTemplate")]
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTitle("WinformTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinformTemplate")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+using System.Runtime.CompilerServices;
+[assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,17 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9e90960-7cf6-418e-b10d-1a57568c2c8a")]
+[assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
+
 //
 //      Major Version
-//      Minor Version
+//      Major Version
 //      Build Number
-//      Revision
+[assembly: AssemblyProduct("WinformTemplate")]
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+[assembly: ComVisible(false)]
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+using System.Runtime.CompilerServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
