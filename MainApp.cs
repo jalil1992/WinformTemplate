1cab6d19c40b2a42f7ca9747e70b61dd441154bb
Jalil <jalilmarkel1992@gmail.com>
2019-03-03 11:32:44 -0500

    Props change  MainApp

--- MainApp.cs
+++ MainApp.cs
@@ -1,10 +1,10 @@
 ﻿using PCKLIB;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 using System.Threading.Tasks;
-using System.Windows.Forms;
-
+        public static void log(string msg, bool msgbox = false)
+            lock (g_locker)
 namespace WinformTemplate
 {
     static class MainApp
@@ -13,16 +13,16 @@ namespace WinformTemplate
         public static object g_locker = new object();
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static UserSetting g_setting;
-
-        [STAThread]
+                    g_main_frm.log(msg);
+using System.Collections.Generic;
         static void Main()
-        {
-            Application.EnableVisualStyles();
+namespace WinformTemplate
+        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             Application.SetCompatibleTextRenderingDefault(false);
             g_main_frm = new MainFrm();
             Application.Run(g_main_frm);
         }
-
+        {
         public static void log(string msg, bool msgbox = false)
         {
             lock (g_locker)
@@ -34,11 +34,11 @@ namespace WinformTemplate
                         MessageBox.Show(msg);
                     g_main_frm.log(msg);
                 }
-                catch (Exception ex)
-                {
+                    if (msgbox)
+                    g_main_frm.log(msg);
                     MessageBox.Show(ex.Message);
-                }
-            }
+        [STAThread]
+        public static void log(string msg, bool msgbox = false)
         }
 
         public static void load_setting()
@@ -47,12 +47,12 @@ namespace WinformTemplate
             if(g_setting == null)
             {
                 g_setting = new UserSetting();
-            }
-        }
-
+            lock (g_locker)
+                {
+        public static UserSetting g_setting;
         public static void save_setting()
         {
             g_setting.Save();
-        }
+        {
     }
 }
