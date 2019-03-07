9fe479fe815faba2f12e58d152ef7cd0e4e4aff1
Jalil <jalilmarkel1992@gmail.com>
2019-03-06 19:38:41 -0500

    Changed logic for  MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -8,7 +8,7 @@
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+            //
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -16,16 +16,16 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-            }
+
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
+            //
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
-        /// </summary>
+            // MainFrm
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
@@ -35,33 +35,33 @@
             this.main_panel.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // main_panel
-            // 
+            //
             this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
             this.main_panel.Controls.Add(this.tableLayoutPanel1);
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.main_panel.Location = new System.Drawing.Point(0, 63);
-            this.main_panel.Name = "main_panel";
-            this.main_panel.Size = new System.Drawing.Size(675, 462);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+        /// the contents of this method with the code editor.
             this.main_panel.TabIndex = 0;
-            // 
+            //
             // tableLayoutPanel1
-            // 
+            //
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.main_panel.ResumeLayout(false);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(675, 462);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
+            // MainFrm
+            //
             // lab_status
-            // 
+            //
             this.lab_status.AutoSize = true;
             this.lab_status.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lab_status.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -71,10 +71,10 @@
             this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
             this.lab_status.Size = new System.Drawing.Size(669, 30);
             this.lab_status.TabIndex = 0;
-            this.lab_status.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
+            this.ResumeLayout(false);
+            //
             // MainFrm
-            // 
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 525);
@@ -89,7 +89,7 @@
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
-        }
+            this.Load += new System.EventHandler(this.MainFrm_Load);
 
         #endregion
 
@@ -98,4 +98,3 @@
         private System.Windows.Forms.Label lab_status;
     }
 }
-
