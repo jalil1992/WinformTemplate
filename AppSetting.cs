823deabf9deca2509426f840654e613ab3250ed6
Jalil <jalilmarkel1992@gmail.com>
2019-03-14 10:31:08 -0400

    Props change  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -4,13 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web.Script.Serialization;
-
+    public class AppSettings<T> where T : new()
 namespace PCKLIB
 {
     public class AppSettings<T> where T : new()
     {
         private const string DEFAULT_FILENAME = "settings.ini";
-
+    {
         public void Save(string fileName = DEFAULT_FILENAME)
         {
             try
@@ -18,37 +18,37 @@ namespace PCKLIB
                 File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
             }
             catch (Exception e)
-            {
-                Console.WriteLine("## App Setting Saving Failed : " + e.Message);
+            try
+            }
             }
         }
-
+}
         public static void Save(T pSettings, string fileName = DEFAULT_FILENAME)
         {
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
         }
-
+using System.Linq;
         public static T Load(string fileName = DEFAULT_FILENAME)
-        {
-            try
-            { 
-                T t = new T();
+    {
+                return default(T);
+            {
+            {
                 if (File.Exists(fileName))
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
                 else
                     return default(T);
-                return t;
+
             }
             catch (Exception e)
             {
                 Console.WriteLine("## App Setting Loading Failed : " + e.Message);
                 return default(T);
-            }
+            try
         }
     }
 
     public class UserSetting : AppSettings<UserSetting>
     {
-        
+    public class AppSettings<T> where T : new()
     }
 }
