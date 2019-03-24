af5e97a6c0a68fa4fff1adb937b96e7a40c60029
Jalil <jalilmarkel1992@gmail.com>
2019-03-24 03:36:59 -0400

    User manage  MainApp

--- MainApp.cs
+++ MainApp.cs
@@ -2,47 +2,47 @@
 using System;
 using System.Collections.Generic;
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-using System.Threading.Tasks;
+                    g_main_frm.log(msg);
         public static void log(string msg, bool msgbox = false)
-            lock (g_locker)
-namespace WinformTemplate
-{
-    static class MainApp
+
+        }
+            Application.SetCompatibleTextRenderingDefault(false);
+            g_setting.Save();
     {
-        public static MainFrm g_main_frm;
+using System;
         public static object g_locker = new object();
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static UserSetting g_setting;
                     g_main_frm.log(msg);
 using System.Collections.Generic;
-        static void Main()
+                    if (msgbox)
 namespace WinformTemplate
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             Application.SetCompatibleTextRenderingDefault(false);
             g_main_frm = new MainFrm();
-            Application.Run(g_main_frm);
-        }
-        {
+
+        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static void log(string msg, bool msgbox = false)
-        {
+namespace WinformTemplate
             lock (g_locker)
-            {
+                {
                 try
                 {
-                    logger.Error(msg);
-                    if (msgbox)
+
+using System;
                         MessageBox.Show(msg);
                     g_main_frm.log(msg);
                 }
-                    if (msgbox)
+        {
                     g_main_frm.log(msg);
                     MessageBox.Show(ex.Message);
         [STAThread]
-        public static void log(string msg, bool msgbox = false)
+        }
         }
 
-        public static void load_setting()
         {
+    }
             g_setting = UserSetting.Load();
             if(g_setting == null)
             {
