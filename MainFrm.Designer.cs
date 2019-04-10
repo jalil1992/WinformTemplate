d2d60dbd04e56e730cb188a68f1c43aff58b3fd3
Jalil <jalilmarkel1992@gmail.com>
2019-04-10 10:20:19 -0400

    Changed logic for  MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -13,7 +13,7 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         }
 
-            if (disposing && (components != null))
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 
             this.main_panel.ResumeLayout(false);
 
@@ -22,7 +22,7 @@
             this.lab_status.AutoSize = true;
         #region Windows Form Designer generated code
         private void InitializeComponent()
-            this.lab_status.Location = new System.Drawing.Point(3, 432);
+            this.Name = "MainFrm";
         /// Required method for Designer support - do not modify
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             // MainFrm
@@ -30,66 +30,66 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
 
             this.main_panel = new System.Windows.Forms.Panel();
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+        #region Windows Form Designer generated code
             this.lab_status = new System.Windows.Forms.Label();
             this.main_panel.SuspendLayout();
-        /// the contents of this method with the code editor.
-            this.SuspendLayout();
             //
+        }
+        /// <summary>
             //
             //
             this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.main_panel.Controls.Add(this.tableLayoutPanel1);
+            this.lab_status.Location = new System.Drawing.Point(3, 432);
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.main_panel.Location = new System.Drawing.Point(0, 63);
+        /// <summary>
             this.Name = "MainFrm";
             this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
             this.main_panel.TabIndex = 0;
         /// the contents of this method with the code editor.
             // tableLayoutPanel1
-        /// Required method for Designer support - do not modify
+{
         private void InitializeComponent()
             this.SuspendLayout();
-            this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
+
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
         }
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(675, 462);
+            this.main_panel.Location = new System.Drawing.Point(0, 63);
+        /// <summary>
             // MainFrm
             //
             // lab_status
             //
             this.lab_status.AutoSize = true;
-            this.lab_status.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lab_status.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lab_status.ForeColor = System.Drawing.Color.PapayaWhip;
+            // MainFrm
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+        /// Required method for Designer support - do not modify
             this.lab_status.Location = new System.Drawing.Point(3, 432);
             this.lab_status.Name = "lab_status";
-        }
-            this.lab_status.Size = new System.Drawing.Size(669, 30);
+        /// <summary>
+            this.main_panel.Location = new System.Drawing.Point(0, 63);
         }
             this.ResumeLayout(false);
             //
-            // MainFrm
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+        }
+            this.main_panel.Location = new System.Drawing.Point(0, 63);
             this.Controls.Add(this.main_panel);
-            this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.main_panel = new System.Windows.Forms.Panel();
             this.ClientSize = new System.Drawing.Size(675, 525);
             this.Controls.Add(this.main_panel);
         }
             this.Name = "MainFrm";
             this.Padding = new System.Windows.Forms.Padding(0, 63, 0, 0);
-    }
-            this.Load += new System.EventHandler(this.MainFrm_Load);
-            this.main_panel.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
+            //
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
         }
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
 
-            //
+{
 
         #endregion
 
