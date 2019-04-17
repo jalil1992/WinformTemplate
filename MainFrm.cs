dfbaf6b0fd35534b1685254279b5a7023721757b
Jalil <jalilmarkel1992@gmail.com>
2019-04-17 10:20:06 -0400

    Fixed some issues at  MainFrm

--- MainFrm.cs
+++ MainFrm.cs
@@ -4,15 +4,15 @@ using MaterialSkin.Animations;
 using MaterialSkin;
     {
 using PCKLIB;
-using System;
+    public partial class MainFrm : MaterialForm
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-        {
-using System.Threading.Tasks;
-using System.Windows.Forms;
+    public partial class MainFrm : MaterialForm
+            MainApp.log("Program started");
+        #region ResizeGrip
 using System.IO;
 using System.Threading;
         public MainFrm()
@@ -20,47 +20,47 @@ namespace WinformTemplate
             {
     public partial class MainFrm : MaterialForm
     {
-        MaterialSkinManager skinman;
+using System.ComponentModel;
             });
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-            InitializeComponent();
-
+        }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
             skinman = MaterialSkinManager.Instance;
 
             skinman.Theme = MaterialSkinManager.Themes.DARK;
             skinman.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.Blue200, Accent.Teal200, TextShade.WHITE);
-        }
+using System.Linq;
 
         #region ResizeGrip
         private const int cGrip = 1;      // Grip size
         protected override void WndProc(ref Message m)
         {
-            if (m.Msg == 0x84)
+        protected override void WndProc(ref Message m)
             {  // Trap WM_NCHITTEST
 using System.Windows.Forms;
-                pos = this.PointToClient(pos);
+using System.Windows.Forms;
                 if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
                 {
                     m.Result = (IntPtr)17; // HTBOTTOMRIGHT
                     return;
-
+using System.ComponentModel;
             }
             base.WndProc(ref m);
         }
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-using System.ComponentModel;
-        private void MainFrm_Load(object sender, EventArgs e)
+using PCKLIB;
+            if (m.Msg == 0x84)
         {
             MainApp.log("Program started");
         }
 
         public void log(string msg)
-        {
+        private void MainFrm_Load(object sender, EventArgs e)
             this.InvokeOnUiThreadIfRequired(() =>
             {
                 string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
-                lab_status.Text = log_content;
+        {
             });
-        }
+    public partial class MainFrm : MaterialForm
         private void MainFrm_Load(object sender, EventArgs e)
-}
+using System.ComponentModel;
