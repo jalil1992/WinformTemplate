6daf42e9f0a61dd751c195be929c05477781b844
Jalil <jalilmarkel1992@gmail.com>
2019-05-12 20:50:07 -0400

    User manage  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,36 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("WinformTemplate")]
-
+using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTitle("WinformTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinformTemplate")]
+// Setting ComVisible to false makes the types in this assembly not visible
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 using System.Runtime.CompilerServices;
-[assembly: AssemblyConfiguration("")]
-
+//
+// by using the '*' as shown below:
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyDescription("")]
 
 //
 //      Major Version
-//      Major Version
+[assembly: AssemblyDescription("")]
 //      Build Number
-[assembly: AssemblyProduct("WinformTemplate")]
+// Setting ComVisible to false makes the types in this assembly not visible
 //
 [assembly: ComVisible(false)]
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-using System.Runtime.CompilerServices;
+[assembly: AssemblyCulture("")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
