4e1c7d977d250f58960dde05e4d5f2bed7cfb181
Jalil <jalilmarkel1992@gmail.com>
2019-05-16 09:59:38 -0400

    User manage  MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -14,7 +14,7 @@
         }
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-
+        private System.Windows.Forms.Panel main_panel;
             this.main_panel = new System.Windows.Forms.Panel();
 
             this.main_panel.SuspendLayout();
@@ -31,35 +31,35 @@
 
             this.main_panel = new System.Windows.Forms.Panel();
         #region Windows Form Designer generated code
-            this.lab_status = new System.Windows.Forms.Label();
+        /// <summary>
             this.main_panel.SuspendLayout();
             //
         }
         /// <summary>
             //
-            //
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
 
             this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
         /// <summary>
             this.lab_status.Name = "lab_status";
             this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
-            this.main_panel.TabIndex = 0;
+
         /// the contents of this method with the code editor.
         #region Windows Form Designer generated code
-{
+    partial class MainFrm
         private void InitializeComponent()
-            this.SuspendLayout();
+
 
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+        }
             this.lab_status.Name = "lab_status";
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.main_panel.Location = new System.Drawing.Point(0, 63);
         /// <summary>
             // MainFrm
-            //
+            // MainFrm
             // lab_status
             //
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -67,14 +67,14 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
     }
             this.lab_status.Location = new System.Drawing.Point(3, 432);
-            this.lab_status.Name = "lab_status";
+
         /// <summary>
             this.main_panel.Location = new System.Drawing.Point(0, 63);
             //
             this.ResumeLayout(false);
             this.main_panel.Location = new System.Drawing.Point(0, 63);
         }
-            this.main_panel.Location = new System.Drawing.Point(0, 63);
+        /// <summary>
         #region Windows Form Designer generated code
             this.main_panel = new System.Windows.Forms.Panel();
             this.ClientSize = new System.Drawing.Size(675, 525);
@@ -88,7 +88,7 @@
 ﻿namespace WinformTemplate
             this.tableLayoutPanel1.ResumeLayout(false);
             this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-
+        }
 {
 
         #endregion
