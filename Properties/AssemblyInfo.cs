d86312ef673cb693a0f8580a3ab15c911cd6733b
Jalil <jalilmarkel1992@gmail.com>
2019-05-18 03:11:37 -0400

    Props change  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,28 +1,28 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("WinformTemplate")]
-using System.Runtime.CompilerServices;
+//
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTitle("WinformTemplate")]
-[assembly: AssemblyDescription("")]
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("WinformTemplate")]
 // Setting ComVisible to false makes the types in this assembly not visible
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+// COM, set the ComVisible attribute to true on that type.
 using System.Runtime.CompilerServices;
 //
-// by using the '*' as shown below:
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+using System.Runtime.CompilerServices;
+// Setting ComVisible to false makes the types in this assembly not visible
 [assembly: AssemblyDescription("")]
-
+//
 //
 //      Major Version
 [assembly: AssemblyDescription("")]
@@ -32,5 +32,5 @@ using System.Runtime.CompilerServices;
 [assembly: ComVisible(false)]
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyCulture("")]
+// General Information about an assembly is controlled through the following
 [assembly: AssemblyFileVersion("1.0.0.0")]
