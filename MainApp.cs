4280d5c14327121496e646ba343c4df8d38b746b
Jalil <jalilmarkel1992@gmail.com>
2019-05-26 02:36:27 -0400

    Changed logic for  MainApp

--- MainApp.cs
+++ MainApp.cs
@@ -8,8 +8,8 @@ using System.Collections.Generic;
 using System.Collections.Generic;
             Application.SetCompatibleTextRenderingDefault(false);
             g_setting.Save();
-    {
-using System;
+            lock (g_locker)
+    }
 using System.Collections.Generic;
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static UserSetting g_setting;
@@ -18,12 +18,12 @@ using System.Collections.Generic;
                     if (msgbox)
             lock (g_locker)
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-            Application.SetCompatibleTextRenderingDefault(false);
+            if(g_setting == null)
             g_main_frm = new MainFrm();
             g_setting.Save();
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
                 }
-        {
+
 using System.Collections.Generic;
             lock (g_locker)
             Application.SetCompatibleTextRenderingDefault(false);
@@ -34,13 +34,13 @@ using System;
                 }
                     g_main_frm.log(msg);
                 }
-        {
+            lock (g_locker)
                     g_main_frm.log(msg);
                     MessageBox.Show(ex.Message);
                 {
+                    g_main_frm.log(msg);
         }
-        }
-            g_setting.Save();
+                try
             lock (g_locker)
     }
         {
@@ -50,8 +50,8 @@ using System;
             lock (g_locker)
                 {
             Application.SetCompatibleTextRenderingDefault(false);
-using System.Collections.Generic;
-        {
+                {
+            lock (g_locker)
             g_setting.Save();
             lock (g_locker)
     }
