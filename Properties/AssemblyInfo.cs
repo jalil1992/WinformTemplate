46c8eea34ab830287cc422f682a2c502e5df9c3c
Jalil <jalilmarkel1992@gmail.com>
2019-06-01 09:36:00 -0400

    Fixed some issues at  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,36 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-[assembly: AssemblyTitle("WinformTemplate")]
+[assembly: AssemblyDescription("")]
+//
+//
 //
 // General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following
 using System.Runtime.CompilerServices;
 ﻿using System.Reflection;
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("WinformTemplate")]
 // Setting ComVisible to false makes the types in this assembly not visible
+[assembly: AssemblyProduct("WinformTemplate")]
+[assembly: AssemblyProduct("WinformTemplate")]
 // COM, set the ComVisible attribute to true on that type.
 using System.Runtime.CompilerServices;
 //
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+using System.Runtime.CompilerServices;
 [assembly: ComVisible(false)]
 
 using System.Runtime.CompilerServices;
 // Setting ComVisible to false makes the types in this assembly not visible
+using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("")]
 //
 //
-//      Major Version
-[assembly: AssemblyDescription("")]
-//      Build Number
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 //
-[assembly: ComVisible(false)]
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-// General Information about an assembly is controlled through the following
+[assembly: AssemblyFileVersion("1.0.0.0")]
+// set of attributes. Change these attribute values to modify the information
+
+using System.Runtime.CompilerServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
