57d6eba4daad7c90843e1c9f15e26c0c358f0abf
Jalil <jalilmarkel1992@gmail.com>
2019-06-07 05:07:57 -0400

    Props change  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 //
 //
 //
-// General Information about an assembly is controlled through the following
-
-// General Information about an assembly is controlled through the following
 using System.Runtime.CompilerServices;
-﻿using System.Reflection;
+
 // Setting ComVisible to false makes the types in this assembly not visible
-[assembly: AssemblyProduct("WinformTemplate")]
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+// Setting ComVisible to false makes the types in this assembly not visible
+//
 [assembly: AssemblyProduct("WinformTemplate")]
 // COM, set the ComVisible attribute to true on that type.
 using System.Runtime.CompilerServices;
 //
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-using System.Runtime.CompilerServices;
-[assembly: ComVisible(false)]
-
-using System.Runtime.CompilerServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 using System.Runtime.CompilerServices;
-[assembly: AssemblyDescription("")]
+// Setting ComVisible to false makes the types in this assembly not visible
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+// set of attributes. Change these attribute values to modify the information
 //
 //
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-// Setting ComVisible to false makes the types in this assembly not visible
+[assembly: AssemblyFileVersion("1.0.0.0")]
 //
 [assembly: AssemblyFileVersion("1.0.0.0")]
 // set of attributes. Change these attribute values to modify the information
-
+// set of attributes. Change these attribute values to modify the information
 using System.Runtime.CompilerServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
