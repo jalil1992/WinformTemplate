907197a2bbf57ad63345d64f066caaac19927bbc
Jalil <jalilmarkel1992@gmail.com>
2019-06-26 07:16:58 -0400

    User manage  MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -1,10 +1,10 @@
 ﻿namespace WinformTemplate
-{
+    }
     partial class MainFrm
     {
         /// <summary>
         /// Required designer variable.
-        /// </summary>
+            this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 
         /// <summary>
@@ -20,13 +20,13 @@
             this.main_panel.SuspendLayout();
         }
             this.lab_status.AutoSize = true;
-        #region Windows Form Designer generated code
+            this.lab_status.Location = new System.Drawing.Point(3, 432);
         private void InitializeComponent()
             this.Name = "MainFrm";
         /// Required method for Designer support - do not modify
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            // MainFrm
-        private void InitializeComponent()
+        /// <summary>
+        /// <summary>
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
 
             this.main_panel = new System.Windows.Forms.Panel();
@@ -48,7 +48,7 @@
         /// the contents of this method with the code editor.
         #region Windows Form Designer generated code
     partial class MainFrm
-        private void InitializeComponent()
+            //
 
 
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -72,7 +72,7 @@
             this.main_panel.Location = new System.Drawing.Point(0, 63);
             //
             this.ResumeLayout(false);
-            this.main_panel.Location = new System.Drawing.Point(0, 63);
+            this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
         }
         /// <summary>
         #region Windows Form Designer generated code
@@ -82,11 +82,11 @@
         }
         /// Required designer variable.
     }
-            //
+        /// <summary>
 ﻿namespace WinformTemplate
         }
 ﻿namespace WinformTemplate
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
             this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
         }
 {
@@ -94,7 +94,7 @@
         #endregion
 
         private System.Windows.Forms.Panel main_panel;
-{
+            this.main_panel = new System.Windows.Forms.Panel();
         private System.Windows.Forms.Panel main_panel;
         /// Required designer variable.
         private System.Windows.Forms.Panel main_panel;
