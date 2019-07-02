93f81c06eb542585ca1cff0cc2311b3623cede55
Jalil <jalilmarkel1992@gmail.com>
2019-07-02 06:12:09 -0400

    Fixed some issues at  MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -2,14 +2,14 @@
     }
     partial class MainFrm
     {
-        /// <summary>
+        private System.Windows.Forms.Panel main_panel;
         /// Required designer variable.
-            this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
+﻿namespace WinformTemplate
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 
         /// <summary>
 
-        /// </summary>
+    partial class MainFrm
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         }
 
@@ -19,36 +19,36 @@
 
             this.main_panel.SuspendLayout();
         }
-            this.lab_status.AutoSize = true;
+            this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lab_status.Location = new System.Drawing.Point(3, 432);
-        private void InitializeComponent()
-            this.Name = "MainFrm";
+        /// <summary>
+    }
         /// Required method for Designer support - do not modify
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-        /// <summary>
+            this.lab_status.Name = "lab_status";
         /// <summary>
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
 
             this.main_panel = new System.Windows.Forms.Panel();
-        #region Windows Form Designer generated code
         /// <summary>
+        /// <summary>
+            this.Controls.Add(this.main_panel);
             this.main_panel.SuspendLayout();
-            //
-        }
+        /// Required method for Designer support - do not modify
         /// <summary>
             //
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.lab_status.Name = "lab_status";
 
+            this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
         /// <summary>
-            this.lab_status.Name = "lab_status";
-            this.tableLayoutPanel1.Controls.Add(this.lab_status, 0, 1);
-
         /// the contents of this method with the code editor.
         #region Windows Form Designer generated code
     partial class MainFrm
-            //
+        }
 
 
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -57,44 +57,44 @@
             this.lab_status.Name = "lab_status";
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.main_panel.Location = new System.Drawing.Point(0, 63);
-        /// <summary>
-            // MainFrm
+            this.main_panel.SuspendLayout();
+            this.ResumeLayout(false);
             // MainFrm
             // lab_status
-            //
+            this.lab_status.Name = "lab_status";
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             // MainFrm
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
     }
-            this.lab_status.Location = new System.Drawing.Point(3, 432);
+
 
         /// <summary>
-            this.main_panel.Location = new System.Drawing.Point(0, 63);
-            //
+        /// Required designer variable.
+        /// <summary>
             this.ResumeLayout(false);
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
         }
         /// <summary>
         #region Windows Form Designer generated code
-            this.main_panel = new System.Windows.Forms.Panel();
-            this.ClientSize = new System.Drawing.Size(675, 525);
+        /// <summary>
+            this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Controls.Add(this.main_panel);
         }
         /// Required designer variable.
     }
-        /// <summary>
+        private System.Windows.Forms.Panel main_panel;
 ﻿namespace WinformTemplate
         }
 ﻿namespace WinformTemplate
             this.main_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
             this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-        }
+            this.ResumeLayout(false);
 {
 
         #endregion
 
         private System.Windows.Forms.Panel main_panel;
-            this.main_panel = new System.Windows.Forms.Panel();
-        private System.Windows.Forms.Panel main_panel;
+            this.lab_status.Name = "lab_status";
+﻿namespace WinformTemplate
         /// Required designer variable.
         private System.Windows.Forms.Panel main_panel;
