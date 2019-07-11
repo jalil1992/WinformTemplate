87039a83709e58fe301c736bc0c4a9146334dd65
Jalil <jalilmarkel1992@gmail.com>
2019-07-11 01:20:55 -0400

    useState related to AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -4,9 +4,9 @@ using System.Linq;
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
 using System.Text;
 using System.Web.Script.Serialization;
-                return default(T);
-                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-﻿using System;
+
+
+            try
         public void Save(string fileName = DEFAULT_FILENAME)
                 else
         private const string DEFAULT_FILENAME = "settings.ini";
@@ -16,19 +16,19 @@ using System.Web.Script.Serialization;
             try
     {
                 File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
-            }
+
                 return default(T);
 using System.Text;
             }
             }
-        }
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-            try
-                return default(T);
+                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
+                else
+                else
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
         }
-    {
-        public static T Load(string fileName = DEFAULT_FILENAME)
+
+            {
     {
                 return default(T);
             {
@@ -37,18 +37,18 @@ using System.Text;
                 else
                 else
                     return default(T);
-
+﻿using System;
             {
             }
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
                     return default(T);
                 return default(T);
             try
-{
+
     {
                 return default(T);
-using System.Linq;
-    {
+                return default(T);
+                return default(T);
     public class AppSettings<T> where T : new()
         public void Save(string fileName = DEFAULT_FILENAME)
         public void Save(string fileName = DEFAULT_FILENAME)
