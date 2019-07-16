10f4fc78dcc8e945b8ca83759a8af542f3c9dd66
Jalil <jalilmarkel1992@gmail.com>
2019-07-16 15:51:36 -0400

    Props change  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 // Setting ComVisible to false makes the types in this assembly not visible
 using System.Runtime.CompilerServices;
 //
-using System.Runtime.CompilerServices;
-//
+// Setting ComVisible to false makes the types in this assembly not visible
+// Setting ComVisible to false makes the types in this assembly not visible
 //
 // set of attributes. Change these attribute values to modify the information
 //
@@ -11,26 +11,26 @@ using System.Runtime.CompilerServices;
 //
 [assembly: AssemblyProduct("WinformTemplate")]
 // COM, set the ComVisible attribute to true on that type.
+// set of attributes. Change these attribute values to modify the information
+//
+using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
-
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("WinformTemplate")]
 using System.Runtime.CompilerServices;
 //
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
-// set of attributes. Change these attribute values to modify the information
-using System.Runtime.CompilerServices;
-//
-﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 //
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-// set of attributes. Change these attribute values to modify the information
+using System.Runtime.CompilerServices;
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 //
 using System.Runtime.CompilerServices;
