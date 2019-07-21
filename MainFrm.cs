186ad6921a267a26e65b67768cbe17680b2d2d9a
Jalil <jalilmarkel1992@gmail.com>
2019-07-21 00:32:08 -0400

    Props change  MainFrm

--- MainFrm.cs
+++ MainFrm.cs
@@ -1,66 +1,66 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using MaterialSkin.Controls;
 using MaterialSkin.Animations;
-using MaterialSkin;
-    {
+        {
+        protected override void WndProc(ref Message m)
 using PCKLIB;
-    public partial class MainFrm : MaterialForm
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-    public partial class MainFrm : MaterialForm
-            MainApp.log("Program started");
-        #region ResizeGrip
-using System.IO;
+    {
 using System.Threading;
+    public partial class MainFrm : MaterialForm
+        {
+            base.WndProc(ref m);
+using System.Threading;
+
         public MainFrm()
 namespace WinformTemplate
             {
-    public partial class MainFrm : MaterialForm
+        protected override void WndProc(ref Message m)
     {
 using System.ComponentModel;
-            });
+using System.Collections.Generic;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-        }
+        public MainFrm()
                 if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
             skinman = MaterialSkinManager.Instance;
-
+    {
             skinman.Theme = MaterialSkinManager.Themes.DARK;
-            skinman.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.Blue200, Accent.Teal200, TextShade.WHITE);
+        {
 using System.Linq;
 
         #region ResizeGrip
-        private const int cGrip = 1;      // Grip size
+            this.InvokeOnUiThreadIfRequired(() =>
         protected override void WndProc(ref Message m)
         {
         protected override void WndProc(ref Message m)
             {  // Trap WM_NCHITTEST
 using System.Windows.Forms;
 using System.Windows.Forms;
-                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+        protected override void WndProc(ref Message m)
                 {
-                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+            this.InvokeOnUiThreadIfRequired(() =>
                     return;
 using System.ComponentModel;
             }
             base.WndProc(ref m);
         }
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-using PCKLIB;
+            skinman = MaterialSkinManager.Instance;
             if (m.Msg == 0x84)
-        {
+    {
             MainApp.log("Program started");
-        }
+using System.ComponentModel;
 
         public void log(string msg)
         private void MainFrm_Load(object sender, EventArgs e)
             this.InvokeOnUiThreadIfRequired(() =>
             {
                 string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
-        {
+using MaterialSkin.Controls;
             });
-    public partial class MainFrm : MaterialForm
+        {
         private void MainFrm_Load(object sender, EventArgs e)
 using System.ComponentModel;
