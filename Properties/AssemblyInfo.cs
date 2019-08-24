310fdbeb21043d8075962e736d89291d5f660334
Jalil <jalilmarkel1992@gmail.com>
2019-08-24 00:26:53 -0400

    Fixed some issues at  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,16 +9,16 @@ using System.Runtime.CompilerServices;
 //
 using System.Runtime.CompilerServices;
 //
-[assembly: AssemblyProduct("WinformTemplate")]
+//
 // COM, set the ComVisible attribute to true on that type.
 // set of attributes. Change these attribute values to modify the information
 //
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
-//
+// set of attributes. Change these attribute values to modify the information
 [assembly: AssemblyProduct("WinformTemplate")]
 using System.Runtime.CompilerServices;
-//
+// Setting ComVisible to false makes the types in this assembly not visible
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
