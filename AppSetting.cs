296f243f40019ba89696ee9fd283d0445ef5cb34
Jalil <jalilmarkel1992@gmail.com>
2019-10-08 05:45:52 -0400

    User manage  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -1,54 +1,53 @@
 ﻿using System;
             }
 using System.Linq;
-                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
+            try
 using System.Text;
 using System.Web.Script.Serialization;
 
 
-            try
+                File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
         public void Save(string fileName = DEFAULT_FILENAME)
                     return default(T);
         }
             try
-            {
-            }
+                return default(T);
+        }
             try
-    {
+            {
                 File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
-                    return default(T);
+            try
                 return default(T);
 using System.Text;
-        public void Save(string fileName = DEFAULT_FILENAME)
+        }
+﻿using System;
+        }
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
+                else
+                File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
+        }
+
+        }
             }
-            {
-                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-                else
-using System.Linq;
-        }
-
-        }
-    {
                 return default(T);
             {
-        }
-
+﻿using System;
+                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
                 return default(T);
-                else
+                    return default(T);
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
 ﻿using System;
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
             }
-                    t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
+            try
                     return default(T);
                 return default(T);
             try
-
+            try
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                 else
             }
-using System.Web.Script.Serialization;
-﻿using System;
-                File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
+            try
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
