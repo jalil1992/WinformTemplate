0dd66670d3ea579502a714e1cbc681b5a9e0ff44
Jalil <jalilmarkel1992@gmail.com>
2019-10-19 16:05:03 -0400

    User manage  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 // Setting ComVisible to false makes the types in this assembly not visible
-using System.Runtime.CompilerServices;
+//
 //
 // Setting ComVisible to false makes the types in this assembly not visible
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -8,29 +8,29 @@ using System.Runtime.CompilerServices;
 //
 //
 using System.Runtime.CompilerServices;
-//
-//
+// Setting ComVisible to false makes the types in this assembly not visible
+// set of attributes. Change these attribute values to modify the information
 // COM, set the ComVisible attribute to true on that type.
-// set of attributes. Change these attribute values to modify the information
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 [assembly: AssemblyProduct("WinformTemplate")]
 using System.Runtime.CompilerServices;
-// Setting ComVisible to false makes the types in this assembly not visible
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-// Setting ComVisible to false makes the types in this assembly not visible
-//
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-//
-using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
+// Setting ComVisible to false makes the types in this assembly not visible
+using System.Runtime.CompilerServices;
+// Setting ComVisible to false makes the types in this assembly not visible
+//
+using System.Runtime.CompilerServices;
+//
+using System.Runtime.CompilerServices;
+// Setting ComVisible to false makes the types in this assembly not visible
+//
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+using System.Runtime.CompilerServices;
+// Setting ComVisible to false makes the types in this assembly not visible
 //
 using System.Runtime.CompilerServices;
