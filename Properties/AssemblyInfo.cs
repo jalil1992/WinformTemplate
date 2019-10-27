d07d0c931430f0150baa318c41d80ed5c1a563e2
Jalil <jalilmarkel1992@gmail.com>
2019-10-27 04:13:40 -0400

    Props change  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,36 @@
 // Setting ComVisible to false makes the types in this assembly not visible
-//
-//
-// Setting ComVisible to false makes the types in this assembly not visible
-// Setting ComVisible to false makes the types in this assembly not visible
-//
 // set of attributes. Change these attribute values to modify the information
-//
-//
-using System.Runtime.CompilerServices;
+// set of attributes. Change these attribute values to modify the information
 // Setting ComVisible to false makes the types in this assembly not visible
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+// Setting ComVisible to false makes the types in this assembly not visible
+using System.Runtime.CompilerServices;
+//
 // set of attributes. Change these attribute values to modify the information
 // COM, set the ComVisible attribute to true on that type.
-// set of attributes. Change these attribute values to modify the information
+using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("WinformTemplate")]
-using System.Runtime.CompilerServices;
+//
 using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
-// Setting ComVisible to false makes the types in this assembly not visible
 using System.Runtime.CompilerServices;
-// Setting ComVisible to false makes the types in this assembly not visible
-//
-using System.Runtime.CompilerServices;
-//
 using System.Runtime.CompilerServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 //
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
 using System.Runtime.CompilerServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 //
+//
 using System.Runtime.CompilerServices;
+//
+//
+//
+using System.Runtime.CompilerServices;
+//
+//
+//
