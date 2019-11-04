774c84a5cf430b403f56299fc173b48873f58420
Jalil <jalilmarkel1992@gmail.com>
2019-11-04 09:34:36 -0500

    User manage  ControlExtensions

--- ControlExtensions.cs
+++ ControlExtensions.cs
@@ -1,19 +1,19 @@
-    //The position is a little special.
+    }
                 }
 using System;
             }
         {
     {
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
-using System.Text;
+            catch (Exception)
     {
         {
 using System.Data;
             }
-        {
+    }
     }
             }
-        public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
+    {
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             if (control.InvokeRequired)
         }
@@ -22,7 +22,7 @@ using System.Data;
     }
         {
         }
-using System;
+    }
         {
             }
             catch (Exception)
@@ -35,43 +35,43 @@ using System;
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
     //This can help you control the scrollbar with scrolling up and down.
     }
-    }
+            }
                 }
-    public static class PanelExtension
+    }
             }
             catch (Exception)
             try
             //pos passed in should be positive
     }
-using System.Windows.Forms;
+                }
         {
 using System.Collections.Generic;
         }
             {
         {
         {
-                }
-    {
+            catch (Exception)
+        {
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             }
     }
     }
+    }
+    }
+                }
+        {
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             }
         public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
-using System.Collections.Generic;
-        {
-    //Position for scrolling up should be negative.
-            }
-            catch (Exception)
     }
-    //Position for scrolling down should be positive
+    {
 using System.Collections.Generic;
                     columnIndex++;
             try
             }
 using System;
             {
-    }
+                }
 using System.Data;
         }
             doubleBufferPropertyInfo.SetValue(control, enable, null);
