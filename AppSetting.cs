2b4bba3621845bb62dabeda8e127f8c2d5eb5376
Jalil <jalilmarkel1992@gmail.com>
2019-11-17 19:37:43 -0500

    Props change  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -1,18 +1,18 @@
-﻿using System;
+                else
             }
 using System.Linq;
             try
 using System.Text;
-using System.Web.Script.Serialization;
+            try
 
 
+                else
                 File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
-        public void Save(string fileName = DEFAULT_FILENAME)
                     return default(T);
         }
             try
                 return default(T);
-        }
+            {
             try
             {
                 File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
@@ -24,7 +24,7 @@ using System.Text;
         }
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-                else
+
                 File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
         }
 
@@ -34,16 +34,16 @@ using System.Text;
             {
 ﻿using System;
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-                return default(T);
+                    return default(T);
                     return default(T);
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
 ﻿using System;
                     t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
-            }
+        }
             try
                     return default(T);
                 return default(T);
-            try
+        }
             try
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
