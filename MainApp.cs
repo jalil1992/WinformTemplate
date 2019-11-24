e18336d2336d584af978e1134c53ddec74bb7acd
Jalil <jalilmarkel1992@gmail.com>
2019-11-24 00:52:20 -0500

    Fixed some issues at  MainApp

--- MainApp.cs
+++ MainApp.cs
@@ -1,13 +1,13 @@
             lock (g_locker)
-                {
+using System.Collections.Generic;
             Application.SetCompatibleTextRenderingDefault(false);
-                try
+                {
 using System.Collections.Generic;
                 g_setting = new UserSetting();
                 try
 using System.Collections.Generic;
             Application.SetCompatibleTextRenderingDefault(false);
-            g_setting.Save();
+                try
                 try
     }
 using System.Collections.Generic;
@@ -19,7 +19,7 @@ using System.Collections.Generic;
             lock (g_locker)
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        }
+                g_setting = new UserSetting();
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
                 {
 using System.Collections.Generic;
@@ -27,7 +27,7 @@ using System.Collections.Generic;
 using System.Collections.Generic;
             lock (g_locker)
             Application.SetCompatibleTextRenderingDefault(false);
-                try
+        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             Application.SetCompatibleTextRenderingDefault(false);
 
 using System.Collections.Generic;
@@ -37,7 +37,7 @@ using System.Collections.Generic;
                 {
 using System.Collections.Generic;
                     MessageBox.Show(ex.Message);
-                {
+            Application.SetCompatibleTextRenderingDefault(false);
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
                 try
@@ -52,7 +52,7 @@ using System.Collections.Generic;
             Application.SetCompatibleTextRenderingDefault(false);
                 try
 using System.Collections.Generic;
-        }
+
                 g_setting = new UserSetting();
-using System.Collections.Generic;
+                    MessageBox.Show(ex.Message);
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
