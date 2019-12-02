7c3415f2675bb211d9f863674fc3ff167f8ea6e5
Jalil <jalilmarkel1992@gmail.com>
2019-12-01 21:19:45 -0500

    Changed logic for  MainFrm

--- MainFrm.cs
+++ MainFrm.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
     public partial class MainFrm : MaterialForm
-        public void log(string msg)
+            base.WndProc(ref m);
 using System.ComponentModel;
         protected override void WndProc(ref Message m)
 using PCKLIB;
             }
 using System.Collections.Generic;
-        #region ResizeGrip
+using System.ComponentModel;
 using System.Data;
-    {
-using PCKLIB;
+            }
+using System.ComponentModel;
     public partial class MainFrm : MaterialForm
-        {
+                string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
             base.WndProc(ref m);
         {
-
+                {
         public MainFrm()
 namespace WinformTemplate
-            {
+                    return;
                     return;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
     public partial class MainFrm : MaterialForm
@@ -25,13 +25,13 @@ namespace WinformTemplate
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
         public MainFrm()
         {
-                string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
-    {
+    public partial class MainFrm : MaterialForm
+using MaterialSkin.Animations;
             skinman.Theme = MaterialSkinManager.Themes.DARK;
         {
 using System.Linq;
 using MaterialSkin.Animations;
-        #region ResizeGrip
+using System.Windows.Forms;
         private void MainFrm_Load(object sender, EventArgs e)
         protected override void WndProc(ref Message m)
         {
@@ -41,24 +41,24 @@ namespace WinformTemplate
 using System.Windows.Forms;
         protected override void WndProc(ref Message m)
                 {
-            this.InvokeOnUiThreadIfRequired(() =>
+        {
                     return;
             MainApp.log("Program started");
             base.WndProc(ref m);
-            base.WndProc(ref m);
+        protected override void WndProc(ref Message m)
 
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
             skinman = MaterialSkinManager.Instance;
-            if (m.Msg == 0x84)
+                    return;
         public void log(string msg)
 using MaterialSkin.Animations;
         public MainFrm()
-
+using PCKLIB;
         public void log(string msg)
 using MaterialSkin.Animations;
             this.InvokeOnUiThreadIfRequired(() =>
                     return;
-                string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
+using MaterialSkin.Animations;
 using MaterialSkin.Controls;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
             base.WndProc(ref m);
