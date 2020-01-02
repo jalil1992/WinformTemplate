72810166753726987e27968997cbbe30b95195ec
Jalil <jalilmarkel1992@gmail.com>
2020-01-02 10:26:15 -0500

    As discussed in the meeting, changed logic at  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -1,53 +1,53 @@
+                    return default(T);
             try
-            try
-            try
+                    return default(T);
             try
         }
                     return default(T);
 ﻿using System;
 
+            try
+            try
+                return default(T);
                 return default(T);
             try
+                    return default(T);
+            try
+            try
+                return default(T);
+            try
+                return default(T);
+            try
+                    return default(T);
+                return default(T);
+        }
+                return default(T);
             }
-                return default(T);
-            try
-            try
-            try
-                return default(T);
-                return default(T);
-            try
-            try
-            try
-            try
-                return default(T);
-        }
-                return default(T);
-﻿using System;
                     return default(T);
 ﻿using System;
-﻿using System;
-        }
-            try
-        }
-                    return default(T);
-        }
-        }
 ﻿using System;
             }
             try
+        }
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+        }
+                return default(T);
+﻿using System;
+            }
+            try
                     return default(T);
-                    return default(T);
-        }
-        }
-        }
+﻿using System;
+            }
+            try
+﻿using System;
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                 return default(T);
-            }
+﻿using System;
                     return default(T);
             try
                 return default(T);
             try
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
-            try
+﻿using System;
