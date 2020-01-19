8aabee7bdaad2ae9d512538ccb6607add73347eb
Jalil <jalilmarkel1992@gmail.com>
2020-01-19 15:21:10 -0500

    Fixed some issues at  MainFrm

--- MainFrm.cs
+++ MainFrm.cs
@@ -1,21 +1,21 @@
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Linq;
     public partial class MainFrm : MaterialForm
             base.WndProc(ref m);
             skinman = MaterialSkinManager.Instance;
 using System.Linq;
 using PCKLIB;
 using System.ComponentModel;
-using MaterialSkin.Animations;
+            base.WndProc(ref m);
 using System.ComponentModel;
 using System.Data;
+using MaterialSkin.Animations;
 using System.ComponentModel;
-using System.ComponentModel;
-    public partial class MainFrm : MaterialForm
+using System.Linq;
                 string log_content = $"{DateTime.Now.ToString("HH:mm:ss")} {msg}";
             base.WndProc(ref m);
         {
-                    return;
-        public MainFrm()
+        {
+using MaterialSkin.Animations;
 namespace WinformTemplate
                     return;
                     return;
@@ -23,21 +23,21 @@ namespace WinformTemplate
     public partial class MainFrm : MaterialForm
         public void log(string msg)
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-        public MainFrm()
-
+                    return;
+namespace WinformTemplate
     public partial class MainFrm : MaterialForm
-using MaterialSkin.Animations;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
             skinman.Theme = MaterialSkinManager.Themes.DARK;
         {
 using System.Linq;
-using MaterialSkin.Animations;
+        protected override void WndProc(ref Message m)
 using System.Windows.Forms;
         {
         protected override void WndProc(ref Message m)
     public partial class MainFrm : MaterialForm
-using MaterialSkin.Animations;
-namespace WinformTemplate
-            this.InvokeOnUiThreadIfRequired(() =>
+                    return;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
         protected override void WndProc(ref Message m)
                 {
@@ -45,22 +45,22 @@ namespace WinformTemplate
                     return;
             MainApp.log("Program started");
                     return;
-        protected override void WndProc(ref Message m)
-
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
+    public partial class MainFrm : MaterialForm
+                {
+    public partial class MainFrm : MaterialForm
 using MaterialSkin.Animations;
                     return;
         public void log(string msg)
 using MaterialSkin.Animations;
-        public MainFrm()
+                    return;
 using PCKLIB;
         public void log(string msg)
 using MaterialSkin.Animations;
-            this.InvokeOnUiThreadIfRequired(() =>
+                    return;
                     return;
 using MaterialSkin.Animations;
 using MaterialSkin.Controls;
-            base.WndProc(ref m);
+using MaterialSkin.Animations;
             base.WndProc(ref m);
         private void MainFrm_Load(object sender, EventArgs e)
                     return;
