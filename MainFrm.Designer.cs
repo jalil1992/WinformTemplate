a658752c02670c4f90ffe6d99f4a6467c11d23a9
Jalil <jalilmarkel1992@gmail.com>
2020-01-29 12:24:09 -0500

    As discussed in the meeting, changed logic at  MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -29,24 +29,24 @@
         /// <summary>
             this.main_panel.SuspendLayout();
             // MainFrm
-            this.main_panel = new System.Windows.Forms.Panel();
-        /// the contents of this method with the code editor.
+    }
+{
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.Controls.Add(this.main_panel);
             this.lab_status.Name = "lab_status";
-        /// Required method for Designer support - do not modify
-    }
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        }
             //
-            this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+{
 
 
         }
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.Controls.Add(this.main_panel);
-            // lab_status
+    }
         /// the contents of this method with the code editor.
-        }
+            // MainFrm
         /// <summary>
         /// <summary>
 ﻿namespace WinformTemplate
@@ -56,13 +56,13 @@
         }
             this.lab_status.Name = "lab_status";
         private System.Windows.Forms.Panel main_panel;
-
+            // lab_status
             this.main_panel.SuspendLayout();
             this.ResumeLayout(false);
-        }
+            this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
             // lab_status
             this.lab_status.Name = "lab_status";
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+
             // MainFrm
         /// Required method for Designer support - do not modify
         /// <summary>
@@ -73,14 +73,14 @@
             this.lab_status.Name = "lab_status";
         private System.Windows.Forms.Panel main_panel;
 {
-        }
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         /// <summary>
 ﻿namespace WinformTemplate
     partial class MainFrm
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Controls.Add(this.main_panel);
+            // MainFrm
         }
-            this.lab_status.Location = new System.Drawing.Point(3, 432);
+    partial class MainFrm
         #region Windows Form Designer generated code
         private System.Windows.Forms.Panel main_panel;
         /// the contents of this method with the code editor.
