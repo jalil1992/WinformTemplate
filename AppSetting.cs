b58d12a9c294163ce50b0af391ffc8fd10c27b7b
Jalil <jalilmarkel1992@gmail.com>
2020-02-09 14:18:56 -0500

    Fixed some issues at  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -1,46 +1,46 @@
                 return default(T);
             try
                     return default(T);
-            try
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                     return default(T);
                     return default(T);
 ﻿using System;
-
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
             try
-            try
+                    return default(T);
                 return default(T);
+                return default(T);
+                return default(T);
+                    return default(T);
+            try
+            try
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+            try
                 return default(T);
             try
                     return default(T);
             try
-            try
-                return default(T);
-            try
-                return default(T);
-            try
-                    return default(T);
-                return default(T);
         }
                 return default(T);
             }
-                    return default(T);
+                return default(T);
 ﻿using System;
 ﻿using System;
             }
+﻿using System;
+        }
             try
         }
-﻿using System;
-        }
                 return default(T);
 ﻿using System;
-                return default(T);
+﻿using System;
             try
                     return default(T);
 ﻿using System;
                 return default(T);
             try
 ﻿using System;
-            try
+﻿using System;
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                 return default(T);
 ﻿using System;
