3f117845f77f075fbec95902da175565b7560841
Jalil <jalilmarkel1992@gmail.com>
2020-02-16 13:41:28 -0500

    As discussed in the meeting, changed logic at  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,31 +1,31 @@
-// Setting ComVisible to false makes the types in this assembly not visible
+using System.Runtime.CompilerServices;
+//
 // set of attributes. Change these attribute values to modify the information
-// set of attributes. Change these attribute values to modify the information
+//
+//
+//
 //
 //
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
-//
-// Setting ComVisible to false makes the types in this assembly not visible
 using System.Runtime.CompilerServices;
-//
-// set of attributes. Change these attribute values to modify the information
+using System.Runtime.CompilerServices;
 // COM, set the ComVisible attribute to true on that type.
-using System.Runtime.CompilerServices;
-[assembly: AssemblyProduct("WinformTemplate")]
 //
-using System.Runtime.CompilerServices;
+//
+//
+//
 // set of attributes. Change these attribute values to modify the information
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
-// Setting ComVisible to false makes the types in this assembly not visible
+//
 //
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
-// Setting ComVisible to false makes the types in this assembly not visible
+// set of attributes. Change these attribute values to modify the information
 //
-//
+// COM, set the ComVisible attribute to true on that type.
 using System.Runtime.CompilerServices;
 //
 //
