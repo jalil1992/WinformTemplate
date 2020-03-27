f2370913b99fb8e6bd01c0be9465d6b0191508fa
Jalil <jalilmarkel1992@gmail.com>
2020-03-27 01:48:16 -0400

    useState related to MainFrm

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -1,7 +1,7 @@
-﻿namespace WinformTemplate
+            // MainFrm
     }
     partial class MainFrm
-
+            this.lab_status.Name = "lab_status";
 
             this.lab_status.Name = "lab_status";
 ﻿namespace WinformTemplate
@@ -16,15 +16,15 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel main_panel;
     }
-    }
+        /// <summary>
             this.main_panel.SuspendLayout();
         private System.Windows.Forms.Panel main_panel;
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lab_status.Location = new System.Drawing.Point(3, 432);
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
             this.lab_status.Name = "lab_status";
         }
             this.ResumeLayout(false);
-            this.main_panel = new System.Windows.Forms.Panel();
+            this.lab_status.Name = "lab_status";
             this.main_panel = new System.Windows.Forms.Panel();
         /// <summary>
             this.main_panel.SuspendLayout();
@@ -37,21 +37,21 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         }
             //
-{
+        }
 
-
+        /// <summary>
         }
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.Controls.Add(this.main_panel);
     }
-        /// the contents of this method with the code editor.
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             // MainFrm
+            this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
         /// <summary>
-        /// <summary>
-﻿namespace WinformTemplate
+    }
 
-﻿namespace WinformTemplate
+        }
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
         }
             this.lab_status.Name = "lab_status";
@@ -68,33 +68,33 @@
         /// <summary>
 
         private System.Windows.Forms.Panel main_panel;
-        /// <summary>
+            this.ResumeLayout(false);
 
             this.lab_status.Name = "lab_status";
         private System.Windows.Forms.Panel main_panel;
 {
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        /// <summary>
-﻿namespace WinformTemplate
+            this.Controls.Add(this.main_panel);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
     partial class MainFrm
             this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             // MainFrm
-        }
-    partial class MainFrm
+{
+
         #region Windows Form Designer generated code
-        private System.Windows.Forms.Panel main_panel;
-        /// the contents of this method with the code editor.
+            this.Controls.Add(this.main_panel);
+﻿namespace WinformTemplate
         }
 ﻿namespace WinformTemplate
 
             this.lab_status.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
             this.lab_status.Name = "lab_status";
-{
+            this.ResumeLayout(false);
             this.lab_status.Name = "lab_status";
     }
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
         /// <summary>
             this.lab_status.Name = "lab_status";
-﻿namespace WinformTemplate
-        /// Required designer variable.
-        private System.Windows.Forms.Panel main_panel;
+
+            // MainFrm
+{
