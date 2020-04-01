9a53335e69e904547ebb0701f48d114633b4380c
Jalil <jalilmarkel1992@gmail.com>
2020-04-01 15:31:58 -0400

    User manage  MainFrm

--- MainFrm.cs
+++ MainFrm.cs
@@ -1,15 +1,15 @@
-        {
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
             base.WndProc(ref m);
                     return;
 using System.Linq;
 using PCKLIB;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
+        {
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Data;
-                    return;
-                    return;
+        {
+    public partial class MainFrm : MaterialForm
 using System.Linq;
                     return;
             base.WndProc(ref m);
@@ -18,7 +18,7 @@ using System.Linq;
         {
 namespace WinformTemplate
                     return;
-using System.ComponentModel;
+using PCKLIB;
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using MaterialSkin.Controls;
                     return;
@@ -26,9 +26,9 @@ using MaterialSkin.Controls;
 using MaterialSkin.Controls;
         {
     public partial class MainFrm : MaterialForm
-                    return;
+using PCKLIB;
             skinman.Theme = MaterialSkinManager.Themes.DARK;
-        {
+                    return;
 using System.Linq;
         {
                     return;
@@ -53,8 +53,8 @@ using MaterialSkin.Animations;
         public void log(string msg)
 using MaterialSkin.Animations;
                     return;
-using PCKLIB;
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
+        {
+                    return;
             base.WndProc(ref m);
                     return;
                     return;
