f6687edf69a6d68791dbabdf70bc89e88c5760bb
Jalil <jalilmarkel1992@gmail.com>
2020-04-22 22:59:23 -0400

    useState related to AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,33 +2,33 @@ using System.Runtime.CompilerServices;
 //
 // set of attributes. Change these attribute values to modify the information
 //
-//
-//
-//
-//
-using System.Runtime.CompilerServices;
-// set of attributes. Change these attribute values to modify the information
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
-//
-//
-//
-//
-//
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-//
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-// set of attributes. Change these attribute values to modify the information
 //
 // COM, set the ComVisible attribute to true on that type.
 using System.Runtime.CompilerServices;
+// set of attributes. Change these attribute values to modify the information
+// COM, set the ComVisible attribute to true on that type.
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 //
 //
+//
+//
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+// set of attributes. Change these attribute values to modify the information
+// COM, set the ComVisible attribute to true on that type.
+using System.Runtime.CompilerServices;
+//
+using System.Runtime.CompilerServices;
 //
 using System.Runtime.CompilerServices;
 //
