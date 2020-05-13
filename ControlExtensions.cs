c805d96c1cbc849787771eaae0354908497882f5
Jalil <jalilmarkel1992@gmail.com>
2020-05-12 21:44:37 -0400

    Changed logic for  ControlExtensions

--- ControlExtensions.cs
+++ ControlExtensions.cs
@@ -1,4 +1,4 @@
-    }
+            }
         {
             }
             }
@@ -6,27 +6,27 @@
                     columnIndex++;
 using System.Data;
     }
-    }
+        public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
             try
-        {
+            try
             }
     }
             }
-            }
-            try
+    }
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             //pos passed in should be positive
             if (control.InvokeRequired)
     }
     {
         {
     }
-        {
+    {
     }
     }
         {
             }
-                }
-    }
+        {
+            try
 using System.Collections.Generic;
                 }
 using System.Collections.Generic;
@@ -35,17 +35,17 @@ using System.Collections.Generic;
     }
     }
     }
-            try
+        {
                 }
     }
     }
     }
-            try
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             //pos passed in should be positive
     }
     }
-            try
-            //pos passed in should be positive
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
+                }
             //pos passed in should be positive
             }
             //pos passed in should be positive
@@ -53,26 +53,26 @@ using System.Collections.Generic;
     }
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             //pos passed in should be positive
-            }
+            //pos passed in should be positive
     }
             try
-    }
-    }
+            }
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
                 }
         {
             try
-            }
+    }
         public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
-    }
+            }
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             }
-            try
-            try
-            try
-using System;
-            {
+    }
+            }
+            }
+    }
+            }
             //pos passed in should be positive
 using System.Data;
     }
-            //pos passed in should be positive
+using System.Collections.Generic;
             //pos passed in should be positive
