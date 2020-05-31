73439abfcd80bf758f9f96719a3a5771f280543b
Jalil <jalilmarkel1992@gmail.com>
2020-05-31 05:40:54 -0400

    User manage  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -2,41 +2,41 @@
             try
                     return default(T);
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
-            try
+                return default(T);
                     return default(T);
 ﻿using System;
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
-            try
-                    return default(T);
-                return default(T);
-                return default(T);
                 return default(T);
                     return default(T);
+                return default(T);
+                return default(T);
+                return default(T);
             try
+                return default(T);
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
-            try
-                return default(T);
-            try
-                    return default(T);
-            try
         }
                 return default(T);
+        }
+                    return default(T);
+                    return default(T);
+        }
+                    return default(T);
             }
                 return default(T);
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
 ﻿using System;
-﻿using System;
-            }
+            try
 ﻿using System;
         }
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
-                return default(T);
-﻿using System;
-﻿using System;
-            try
                     return default(T);
 ﻿using System;
+﻿using System;
+        }
+            try
+                return default(T);
                 return default(T);
             try
 ﻿using System;
