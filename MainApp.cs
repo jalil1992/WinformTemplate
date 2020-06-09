785f3d86e5b626d5638818b3456a835263c2a84a
Jalil <jalilmarkel1992@gmail.com>
2020-06-09 06:21:50 -0400

    useState related to MainApp

--- MainApp.cs
+++ MainApp.cs
@@ -27,7 +27,7 @@ using System.Collections.Generic;
 using System.Collections.Generic;
             lock (g_locker)
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-using System.Collections.Generic;
+                {
             lock (g_locker)
 using System.Collections.Generic;
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
@@ -36,12 +36,12 @@ using System.Collections.Generic;
 using System.Collections.Generic;
                 {
                 try
-                    MessageBox.Show(ex.Message);
+            lock (g_locker)
             lock (g_locker)
 
                 try
                 g_setting = new UserSetting();
-            lock (g_locker)
+            if(g_setting == null)
                     MessageBox.Show(ex.Message);
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 using System.Collections.Generic;
@@ -51,8 +51,8 @@ using System.Collections.Generic;
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
                 try
 using System.Collections.Generic;
-
                 try
+
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         }
