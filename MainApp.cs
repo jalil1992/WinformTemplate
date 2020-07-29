db5a7235c98115a6dca4f3b61b92483466db3109
Jalil <jalilmarkel1992@gmail.com>
2020-07-29 05:13:39 -0400

    Changed logic for  MainApp

--- MainApp.cs
+++ MainApp.cs
@@ -1,4 +1,4 @@
-                try
+using System.Collections.Generic;
             Application.SetCompatibleTextRenderingDefault(false);
 using System.Collections.Generic;
                 try
@@ -6,10 +6,10 @@ using System.Collections.Generic;
 using System.Collections.Generic;
                 try
 using System.Collections.Generic;
-using System.Collections.Generic;
+                try
                 {
                 try
-        }
+            lock (g_locker)
                 try
                 try
 using System.Collections.Generic;
@@ -17,38 +17,38 @@ using System.Collections.Generic;
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 using System.Collections.Generic;
             lock (g_locker)
-            lock (g_locker)
-                try
+using System.Collections.Generic;
+using System.Collections.Generic;
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 using System.Collections.Generic;
             lock (g_locker)
-            lock (g_locker)
+            if(g_setting == null)
 using System.Collections.Generic;
-            lock (g_locker)
+using System.Collections.Generic;
             Application.SetCompatibleTextRenderingDefault(false);
                 {
-            lock (g_locker)
+                {
 using System.Collections.Generic;
             lock (g_locker)
 using System.Collections.Generic;
+            lock (g_locker)
 using System.Collections.Generic;
-        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 using System.Collections.Generic;
+                try
 using System.Collections.Generic;
             lock (g_locker)
             lock (g_locker)
-
                 try
             if(g_setting == null)
             if(g_setting == null)
                     MessageBox.Show(ex.Message);
-                g_setting = new UserSetting();
+                try
             Application.SetCompatibleTextRenderingDefault(false);
             lock (g_locker)
 using System.Collections.Generic;
-        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+                try
+                try
             lock (g_locker)
                 {
                 try
