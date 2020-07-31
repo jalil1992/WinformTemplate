f16c5aec8cab1cbc7353f6eb9b02b572e348ab11
Jalil <jalilmarkel1992@gmail.com>
2020-07-31 00:34:50 -0400

    User manage  ControlExtensions

--- ControlExtensions.cs
+++ ControlExtensions.cs
@@ -9,31 +9,31 @@ using System.Data;
 using System.Collections.Generic;
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
-            }
+    }
             //pos passed in should be positive
     }
-            }
+                }
     }
             //pos passed in should be positive
             if (control.InvokeRequired)
             }
     {
-            //pos passed in should be positive
+    }
     }
     }
     }
     }
         {
-            }
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
         {
     }
 using System.Collections.Generic;
             }
 using System.Collections.Generic;
+        {
+    }
+    }
             }
-    }
-    }
-    }
     }
     }
                 }
@@ -43,36 +43,36 @@ using System.Collections.Generic;
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             }
     }
-using System.Collections.Generic;
+            }
             //pos passed in should be positive
             //pos passed in should be positive
             }
             }
-            //pos passed in should be positive
+            }
             }
             }
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
-        {
+    }
             //pos passed in should be positive
     }
                 }
     }
     }
-            }
+    }
     }
             try
     {
-        public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
+            }
             }
             using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
             }
     }
+                }
             }
-                    columnIndex++;
             }
             }
             if (control.InvokeRequired)
             if (control.InvokeRequired)
-            try
     }
+            using (Control c = new Control() { Parent = p, Height = 1, Top = p.ClientSize.Height + pos })
         {
