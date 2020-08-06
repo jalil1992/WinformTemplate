cef820d3b78b787d4729dd6bba70f77e1e7828f1
Jalil <jalilmarkel1992@gmail.com>
2020-08-06 02:58:37 -0400

    Changed logic for  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,36 @@
+using System.Runtime.CompilerServices;
 //
-//
-// set of attributes. Change these attribute values to modify the information
+using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
 using System.Runtime.CompilerServices;
 //
 // COM, set the ComVisible attribute to true on that type.
-using System.Runtime.CompilerServices;
-// set of attributes. Change these attribute values to modify the information
-//
+// COM, set the ComVisible attribute to true on that type.
 //
 using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-//
-//
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-//
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-// set of attributes. Change these attribute values to modify the information
-//
-//
 //
 using System.Runtime.CompilerServices;
 //
 using System.Runtime.CompilerServices;
 //
 //
+//
 using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+// COM, set the ComVisible attribute to true on that type.
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+//
+//
+//
+//
