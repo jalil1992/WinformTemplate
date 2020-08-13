df03c17d9493a8c283dcdfa0c1e9dbce224ff96f
Jalil <jalilmarkel1992@gmail.com>
2020-08-13 16:51:50 -0400

    As discussed in the meeting, changed logic at  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -1,20 +1,20 @@
-                return default(T);
+                    return default(T);
             try
                     return default(T);
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                 return default(T);
                     return default(T);
 ﻿using System;
-            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+                return default(T);
                 return default(T);
                     return default(T);
                 return default(T);
                 return default(T);
                 return default(T);
             try
+            }
+﻿using System;
                 return default(T);
-            try
-            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
         }
                 return default(T);
         }
@@ -33,21 +33,21 @@
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                     return default(T);
 ﻿using System;
-﻿using System;
+        }
         }
             try
                 return default(T);
                 return default(T);
             try
 ﻿using System;
-﻿using System;
                 return default(T);
                 return default(T);
-﻿using System;
+                return default(T);
+                return default(T);
                     return default(T);
             try
-                return default(T);
+﻿using System;
             try
             try
-            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+            try
 ﻿using System;
