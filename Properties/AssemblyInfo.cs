1256f91f04bbb455d1fbf0b35f43ec844b3f1024
Jalil <jalilmarkel1992@gmail.com>
2020-08-27 08:34:57 -0400

    User manage  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,30 +7,30 @@ using System.Runtime.CompilerServices;
 //
 // COM, set the ComVisible attribute to true on that type.
 // COM, set the ComVisible attribute to true on that type.
-//
-using System.Runtime.CompilerServices;
-//
-using System.Runtime.CompilerServices;
-//
-using System.Runtime.CompilerServices;
-//
-//
-//
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 //
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
 //
 // COM, set the ComVisible attribute to true on that type.
+//
+using System.Runtime.CompilerServices;
+//
+//
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+//
+using System.Runtime.CompilerServices;
+//
+//
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
+using System.Runtime.CompilerServices;
 //
 //
-//
-//
+using System.Runtime.CompilerServices;
