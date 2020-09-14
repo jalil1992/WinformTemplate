167a069772e40e061393d71ddf20ee4108bec062
Jalil <jalilmarkel1992@gmail.com>
2020-09-14 07:39:07 -0400

    Fixed some issues at  AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,26 +6,26 @@ using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
 // COM, set the ComVisible attribute to true on that type.
+//
 // COM, set the ComVisible attribute to true on that type.
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-//
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-using System.Runtime.CompilerServices;
-//
-//
 // COM, set the ComVisible attribute to true on that type.
 //
 using System.Runtime.CompilerServices;
-//
-//
-using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
 //
+//
 using System.Runtime.CompilerServices;
 //
+//
+//
+//
+//
+//
+//
+//
+using System.Runtime.CompilerServices;
+// COM, set the ComVisible attribute to true on that type.
 //
 using System.Runtime.CompilerServices;
 using System.Runtime.CompilerServices;
