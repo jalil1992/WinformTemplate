f95722d9748b8f31334974da78114b3dc14253ae
Jalil <jalilmarkel1992@gmail.com>
2020-09-19 16:28:16 -0400

    Fixed some issues at  AppSetting

--- AppSetting.cs
+++ AppSetting.cs
@@ -1,34 +1,34 @@
-                    return default(T);
+                return default(T);
             try
                     return default(T);
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                 return default(T);
                     return default(T);
-﻿using System;
+                return default(T);
                 return default(T);
                 return default(T);
                     return default(T);
                 return default(T);
                 return default(T);
                 return default(T);
-            try
-            }
-﻿using System;
-                return default(T);
-        }
-                return default(T);
-        }
-                    return default(T);
-                    return default(T);
-        }
-                    return default(T);
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
             }
                 return default(T);
+                return default(T);
+        }
+                return default(T);
+﻿using System;
+                    return default(T);
+        }
+        }
+                    return default(T);
+            try
+                    return default(T);
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
 ﻿using System;
             try
 ﻿using System;
-        }
+                return default(T);
             try
             File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
                     return default(T);
@@ -38,15 +38,15 @@
             try
                 return default(T);
                 return default(T);
-            try
+                    return default(T);
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+                return default(T);
+                return default(T);
 ﻿using System;
-                return default(T);
-                return default(T);
-                return default(T);
                 return default(T);
                     return default(T);
             try
-﻿using System;
+                    return default(T);
             try
             try
             try
