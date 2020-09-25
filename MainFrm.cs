cdb1c561859db9848e36f48ad1136202ea9c4620
Jalil <jalilmarkel1992@gmail.com>
2020-09-25 09:50:57 -0400

    useState related to MainFrm

--- MainFrm.cs
+++ MainFrm.cs
@@ -1,15 +1,15 @@
                     return;
                     return;
             base.WndProc(ref m);
+using System.ComponentModel;
                     return;
-                {
     public partial class MainFrm : MaterialForm
                     return;
-                    return;
+        {
                     return;
 using System.Data;
+                    return;
         {
-    public partial class MainFrm : MaterialForm
         {
                     return;
                     return;
@@ -28,19 +28,6 @@ using System.ComponentModel;
     public partial class MainFrm : MaterialForm
                     return;
                     return;
-        {
-                    return;
-        {
-using MaterialSkin.Controls;
-                    return;
-                    return;
-                    return;
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
-using System.ComponentModel;
-                    return;
-                    return;
-using MaterialSkin.Controls;
-                    return;
 using PCKLIB;
                     return;
         {
@@ -48,18 +35,31 @@ using PCKLIB;
                     return;
                     return;
                     return;
+                    return;
+                    return;
+                    return;
+                    return;
+using MaterialSkin.Controls;
+                    return;
+                    return;
+                    return;
+                    return;
+                    return;
+                    return;
+                    return;
+    public partial class MainFrm : MaterialForm
 using MaterialSkin.Animations;
                     return;
         public void log(string msg)
                 {
-                {
                     return;
-                    return;
+        {
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
                     return;
 using MaterialSkin.Controls;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
                     return;
                     return;
-using PCKLIB;
                     return;
                     return;
                     return;
